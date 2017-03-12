a231507e25a4ee1bbbcd0d6883a8e454bd5cef2e
unushessus <leopoldhess@gmail.com>
2017-03-12 21:33:05 +0100

    Completed Assignment no. 2 (Week 3)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two function together creat a special object for storing inverse of a matrix
+## The inverse is stored in memory and can be retrieved at any time
+## It does not haveto be calculated every time you need it
 
-## Write a short comment describing this function
+## This function creates a special object containing the functions to
+## set and get the value of a matrix, and set and get its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      i <- NULL 			## reserves a variable for the inverse
+      set<-function(y){
+            x <<- y    			## sets the value of x in parent environment
+            i <<- NULL 			## resets inverse
+      }
+      get <- function() x                         	## retrieves x
+      setinverse <- function(inverse) i<<-inverse 	## sets inverse
+      getinverse <- function() i                  	## retrieves inverse
+      
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse) 			## names for the functions
 }
 
 
-## Write a short comment describing this function
+## The second function uses the sub-functions in makeCacheMatrix, calling them by name
+## It calculates the inverse of a matrix, unless the value is already cached
+## In that case it retrieves it from memory
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      i <- x$getinverse() 			## gets inverse from the makeCacheMatrix object
+      if(!is.null(i)) {   			## checks if the inverse is cached, if true, returns it
+            message("getting cached data")
+            return(i)
+      }                  			## if the inverse is not cached:
+      data <- x$get()    			## gets the matrix
+      i <- solve(data, ...) 			## assings its inverse to i
+      x$setinverse(i)    			## stores i in cache
+      i                  			## and returns it
 }
